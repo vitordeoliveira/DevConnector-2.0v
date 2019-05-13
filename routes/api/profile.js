04934303e4af6f9f217e0b9e0e81f7aa4cf7c171
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2019-05-13 11:38:29 -0300

    profile part 1

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
-// @route   Get api/profile
-// @desc    Test Route
-// @access  Public
-router.get("/", (req, res) => res.send("Profile Route"));
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// @route   Get api/profile/me
+// @desc    Get current users profile
+// @access  Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: "there is no profile for this user" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
 
 module.exports = router;
